1f5d5736cdd3d34697d142dcea614396d7c5306e
MaggiePeterson <margaret.peterson@gmail.com>
2021-05-07 15:33:52 -0700

    first commit

---
+++ src/main/include/Robot.h
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <rev/CANSparkMax.h> 
+
+#define rMotorFrontID 15
+#define rMotorBackID 16
+#define lMotorFrontID 2
+#define lMotorBackID 3
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  rev::CANSparkMax * lMotorFront = new rev::CANSparkMax(lMotorFrontID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax * lMotorBack = new rev::CANSparkMax(lMotorBackID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax * rMotorBack = new rev::CANSparkMax(rMotorBackID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax * rMotorFront = new rev::CANSparkMax(rMotorFrontID, rev::CANSparkMax::MotorType::kBrushless);
+
+};
